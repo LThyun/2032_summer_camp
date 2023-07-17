ea3c85018889d63cb85c4267588bf35d0762b01d
taehyun <snsn0547@nate.com>
2023-07-17 13:55:43 +0900

    demux

--- demux/src/rtl/demux.v
+++ demux/src/rtl/demux.v
@@ -20,28 +20,28 @@ output reg dout;
 
 always @(*) begin
 if(a == 1'b0 && b == 1'b0) begin
-    aout <= 1'b0;
-    bout <= 1'b0;
-    cout <= 1'b0;
-    dout <= 1'b1;
+    aout = 1'b0;
+    bout = 1'b0;
+    cout = 1'b0;
+    dout = 1'b1;
 end
 else if (a == 1'b0 && b == 1'b1) begin
-    aout <= 1'b0;
-    bout <= 1'b0;
-    cout <= 1'b1;
-    dout <= 1'b0;
+    aout = 1'b0;
+    bout = 1'b0;
+    cout = 1'b1;
+    dout = 1'b0;
 end
 else if (a == 1'b1 && b == 1'b0) begin
-    aout <= 1'b0;
-    bout <= 1'b1;
-    cout <= 1'b0;
-    dout <= 1'b0;
+    aout = 1'b0;
+    bout = 1'b1;
+    cout = 1'b0;
+    dout = 1'b0;
 end
 else if (a == 1'b1 && b == 1'b1) begin
-    aout <= 1'b1;
-    bout <= 1'b0;
-    cout <= 1'b0;
-    dout <= 1'b0;
+    aout = 1'b1;
+    bout = 1'b0;
+    cout = 1'b0;
+    dout = 1'b0;
 end
 end
 
